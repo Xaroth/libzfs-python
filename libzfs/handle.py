8622ab28e9ebb60f3ecc1c48a15feab0d207bab2
Xaroth Brook <devnull@xaroth.nl>
2014-04-23 02:16:36 +0200

    -Change: LibZFSHandle should only allocate 1 global pointer.
    so why even offer the option to do more...

--- libzfs/handle.py
+++ libzfs/handle.py
@@ -10,49 +10,37 @@ class LibZFSHandle(object):
     """
 
     _global_ptr = None
-
-    def __init__(self):
-        self._ptr = None
-
-    def __enter__(self):
-        if not self._ptr:
-            self._ptr = c_libzfs.libzfs_init()
-
-    def __exit__(self, exc_type = None, exc_val = None, exc_tb = None):
-        if self._ptr is not None:
-            c_libzfs.libzfs_fini(self._ptr)
-            self._ptr = None
+    _count = 0
 
     @classmethod
-    def init(cls, auto_release = False):
-        """
-        Initializes a handle with libzfs
+    def _add_ref(cls):
+        cls._count += 1
+        if cls._global_ptr is None:
+            cls._global_ptr = c_libzfs.libzfs_init()
+        return cls._global_ptr
 
-        :param auto_release: use cffi's gc function to auto-release the handle (not recommended). Default False.
-        :type auto_release: bool.
-        """
-        if not cls._global_ptr:
-            if auto_release:
-                def _fini(ptr):
-                    if ptr == cls._global_ptr:
-                        cls._global_ptr = None
-                    c_libzfs.libzfs_fini(ptr)
-                cls._global_ptr = ffi_libzfs.gc(c_libzfs.libzfs_init(), _fini)
-            else:
-                cls._global_ptr = c_libzfs.libzfs_init()
+    @classmethod
+    def _del_ref(cls):
+        cls._count -= 1
+        if cls._count < 1 and cls._global_ptr is not None:
+            cls._count = 0
+            c_libzfs.libzfs_fini(cls._global_ptr)
+            cls._global_ptr = None
+
+    def __enter__(self):
+        return LibZFSHandle._add_ref()
+
+    def __exit__(self, exc_type = None, exc_val = None, exc_tb = None):
+        LibZFSHandle._del_ref()
+
+    @classmethod
+    def refcount(cls):
+        return cls._count
+
+    @classmethod
+    def init(cls):
+        return LibZFSHandle._add_ref()
 
     @classmethod
     def fini(cls):
-        """
-        Closes the handle with libzfs
-
-        .. note::
-           If the handle was created with ``auto_release`` set to True, this should not need to be called.
-
-        .. note::
-           There should be no harm to calling this function more than once.
-           Once the global handle has been closed, this function does nothing until a new one is opened.
-        """
-        if cls._global_ptr is not None:
-            c_libzfs.libzfs_fini(cls._global_ptr)
-            cls._global_ptr = None
+        LibZFSHandle._del_ref()
