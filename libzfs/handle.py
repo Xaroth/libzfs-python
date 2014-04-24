0eebf1cc09f188b0e3c8208dd158e71cbf352ef0
Xaroth Brook <devnull@xaroth.nl>
2014-04-24 16:44:18 +0200

    -Add: decorators for LibZFSHandle to require or auto-claim a active handle

--- libzfs/handle.py
+++ libzfs/handle.py
@@ -1,7 +1,13 @@
 from .bindings import manager
+import functools
 
 c_libzfs = manager.libzfs
 ffi_libzfs = manager.libzfs_ffi
+
+
+class NoHandleException(Exception):
+    def __init__(self):
+        super(NoHandleException, self).__init__("There are no open libzfs handles")
 
 
 class LibZFSHandle(object):
@@ -32,6 +38,23 @@ class LibZFSHandle(object):
 
     def __exit__(self, exc_type = None, exc_val = None, exc_tb = None):
         LibZFSHandle._del_ref()
+
+    @classmethod
+    def requires_refcount(cls, func):
+        @functools.wraps(func)
+        def requires_active_handle(*args, **kwargs):
+            if cls.refcount() == 0:
+                raise NoHandleException()  # You probably want to encase your code in a 'with LibZFSHandle():' block...
+            return func(*args, **kwargs)
+        return requires_active_handle
+
+    @classmethod
+    def auto(cls, func):
+        @functools.wraps(func)
+        def auto_claim_handle(*args, **kwargs):
+            with cls():
+                return func(*args, **kwargs)
+        return auto_claim_handle
 
     @classmethod
     def refcount(cls):
