16f85430a9d8574be32eca2f2c689068fcd6c87f
Xaroth Brook <xaroth@xaroth.nl>
2015-03-18 10:24:07 +0100

    -Add: New loader system (parts of the utility header files copied and slightly adjusted from pycparser).
    -Change: rework LibZFSHandle to use the new loader system already.

--- libzfs/__init__.py
+++ libzfs/__init__.py
@@ -1,29 +1,8 @@
-from ._version import __version__, __version_info__
+__version_info__ = ('0', '0', '1')
+__version__ = '.'.join(__version_info__)
 
-VERSION = __version__
+from .manager import default_manager as bindings
 
-try:
-    from ._zfsversion import libzfs_version
-except ImportError:
-    from .utils.version import detect_libzfs_version
-    # We default back to the latest libzfs version
-    libzfs_version = detect_libzfs_version()
-
-
-from .handle import LibZFSHandle
-from .zpool import ZPool
-
-
-from .bindings import enums
 __all__ = [
-    'enums',
-
-    'VERSION',
-    '__version__',
-    '__version_info__',
-    'libzfs_version',
-
-    'LibZFSHandle',
-
-    'ZPool',
+    'bindings',
 ]
