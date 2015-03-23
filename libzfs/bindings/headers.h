e60e2c5d9b15f7b6f7f122617491737341547f74
Xaroth Brook <xaroth@xaroth.nl>
2015-03-23 14:53:43 +0100

    -Add: workaround for zpool_get_config.

--- libzfs/bindings/headers.h
+++ libzfs/bindings/headers.h
@@ -1,1 +1,1 @@
-
+nvlist_t *zpool_get_old_config(zpool_handle_t *);
--- libzfs/bindings/verify.c
+++ libzfs/bindings/verify.c
@@ -2,3 +2,16 @@
 #include <libnvpair.h>
 #include <sys/fs/zfs.h>
 #include <sys/types.h>
+
+/*
+ * A small workaround.
+ *  python-cffi does not allow ffi.addressof(<pointer>).. so using libzfs'
+ *  method of newconfig = zpool_get_config(zhp, &oldconfig) won't work nicely
+ *  instead, we create our own zpool_get_old_config function that pretty
+ *  much does the same.
+ */
+nvlist_t *zpool_get_old_config(zpool_handle_t *zhp) {
+    nvlist_t *oldconfig, *newconfig;
+    newconfig = zpool_get_config(zhp, &oldconfig);
+    return (oldconfig);
+}
