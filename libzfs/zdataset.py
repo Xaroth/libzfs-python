b8adf2f17e217baefa61415fe19ca3c25ae6a258
Xaroth Brook <xaroth@xaroth.nl>
2015-03-24 16:44:19 +0100

    -Add: Properties for ZDatasets.

--- libzfs/zdataset.py
+++ libzfs/zdataset.py
@@ -6,9 +6,17 @@ libzfs = bindings.libzfs
 ffi = bindings.ffi
 
 zfs_type_t = bindings['zfs_type_t']
+zprop_type_t = bindings['zprop_type_t']
+zfs_prop_t = bindings['zfs_prop_t']
+zprop_source_t = bindings['zprop_source_t']
+ZFS_MAXNAMELEN = bindings['ZFS_MAXNAMELEN']
 
 
 class ZDataset(object):
+    _properties = None
+    _propertysources = None
+    _propertynames = None
+
     def __init__(self, hdl):
         self._hdl = hdl
         self._type = zfs_type_t(libzfs.zfs_get_type(hdl))
@@ -32,6 +40,58 @@ class ZDataset(object):
     @property
     def hdl(self):
         return self._hdl
+
+    @LibZFSHandle.requires_refcount
+    @LibZFSHandle.auto
+    def refresh_properties(self):
+        self._properties = {}
+        self._propertysources = {}
+        self._propertynames = {}
+
+        for prop in zfs_prop_t:
+            if prop >= zfs_prop_t.ZFS_NUM_PROPS:
+                continue
+            if not bool(libzfs.zfs_prop_valid_for_type(int(prop), int(self._type), boolean_t(False))):
+                continue
+
+            sourceholder = ffi.new('zprop_source_t *')
+            statbuf = ffi.new('char [%s]' % ZFS_MAXNAMELEN)
+            ptype = zprop_type_t(libzfs.zfs_prop_get_type(int(prop)))
+
+            if ptype == zprop_type_t.PROP_TYPE_NUMBER:
+                holder = ffi.new("uint64_t *")
+                res = libzfs.zfs_prop_get_numeric(self.hdl, int(prop), holder, sourceholder, statbuf, ZFS_MAXNAMELEN)
+                if res == 0:
+                    value = int(holder[0])
+            else:
+                holder = ffi.new("char [%s]" % ZFS_MAXNAMELEN)
+                res = libzfs.zfs_prop_get(self.hdl, int(prop), holder, ZFS_MAXNAMELEN, sourceholder, statbuf,
+                                          ZFS_MAXNAMELEN, boolean_t(True))
+                if res == 0:
+                    value = ffi.string(holder)
+
+            name = ffi.string(libzfs.zfs_prop_to_name(int(prop)))
+            self._propertysources[prop] = zprop_source_t(sourceholder[0])
+            self._propertynames[prop] = name
+            self._properties[prop] = value
+
+    @property
+    def properties(self):
+        if self._properties is None:
+            self.refresh_properties()
+        return self._properties
+
+    @property
+    def propertysources(self):
+        if self._propertysources is None:
+            self.refresh_properties()
+        return self._propertysources
+
+    @property
+    def propertynames(self):
+        if self._propertynames is None:
+            self.refresh_properties()
+        return self._propertynames
 
     @classmethod
     @LibZFSHandle.requires_refcount
