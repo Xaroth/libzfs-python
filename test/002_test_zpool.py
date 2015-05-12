69efdb2669095293e5d6d6f0e8c19bdb0a55c135
Xaroth Brook <xaroth@xaroth.nl>
2015-05-12 10:13:48 +0200

    -Add: Test cases for the easier-to-access data from the helper classes.

--- test/002_test_zpool.py
+++ test/002_test_zpool.py
@@ -2,6 +2,7 @@ import unittest
 import os
 from .test_utils import _LibZFSHandleCase
 from libzfs.zpool import ZPool, zpool_prop_t
+from libzfs import bindings
 
 LIBZFS_TEST_POOL = os.environ.get("LIBZFS_TEST_POOL", False)
 
@@ -23,7 +24,13 @@ class Test_ZPool(_LibZFSHandleCase):
         assert props.get(zpool_prop_t.ZPOOL_PROP_NAME) == LIBZFS_TEST_POOL
         assert props.get(zpool_prop_t.ZPOOL_PROP_SIZE) > 0
 
-    def test_004_get_zpool_config(self):
+    def test_004_get_zpool_properties_easy_access(self):
+        pool = ZPool.get(name=LIBZFS_TEST_POOL)
+        props = pool.properties
+        assert props.name == props.get(zpool_prop_t.ZPOOL_PROP_NAME, None)
+        assert props.size == props.get(zpool_prop_t.ZPOOL_PROP_SIZE, None)
+
+    def test_005_get_zpool_config(self):
         pool = ZPool.get(name=LIBZFS_TEST_POOL)
         config = pool.config
         pool.refresh_stats()
@@ -31,3 +38,10 @@ class Test_ZPool(_LibZFSHandleCase):
         assert len(config.keys()) > 0
         assert len(oldconfig.keys()) > 0
         assert config == oldconfig
+
+    def test_006_get_zpool_config_easy_access(self):
+        from datetime import datetime
+        pool = ZPool.get(name=LIBZFS_TEST_POOL)
+        config = pool.config
+        assert isinstance(config.initial_load_time, datetime)
+        assert config.name == config.get(bindings['ZPOOL_CONFIG_POOL_NAME'], None)
