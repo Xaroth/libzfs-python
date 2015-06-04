bef8d2c2b4376623a68641fe7fb5c7183e29677c
Xaroth Brook <xaroth@xaroth.nl>
2015-06-04 09:36:40 +0200

    -Add: Allow jsonifying our enums.

--- libzfs/zpool.py
+++ libzfs/zpool.py
@@ -4,6 +4,7 @@ from .handle import LibZFSHandle
 from .utils.conversion import boolean_t
 
 from datetime import datetime
+from enum import Enum
 
 libzfs = bindings.libzfs
 ffi = bindings.ffi
@@ -44,7 +45,14 @@ def _key_getter(key, default=None, transform=None, name=None):
     return property(_getter)
 
 
-def jsonify(o, max_depth=-1):
+PARSE_KEEP = 0
+PARSE_NAME = 1
+PARSE_NAME_LONG = 2
+PARSE_VALUE = 3
+PARSE_BOTH = 4
+
+
+def jsonify(o, max_depth=-1, parse_enums=PARSE_KEEP):
     """
     Walks through object o, and attempts to get the property instead of the key, if available.
     This means that for our VDev objects we can easily get a dict of all the 'parsed' values.
@@ -53,11 +61,21 @@ def jsonify(o, max_depth=-1):
         return o
     max_depth -= 1
     if isinstance(o, dict):
-        return {key: jsonify(getattr(o, key, value), max_depth) for key, value in o.items()}
+        return {key: jsonify(getattr(o, key, value), max_depth=max_depth, parse_enums=parse_enums)
+                for key, value in o.items()}
     elif isinstance(o, list):
-        return [jsonify(x, max_depth) for x in o]
+        return [jsonify(x, max_depth=max_depth, parse_enums=parse_enums) for x in o]
     elif isinstance(o, tuple):
-        return (jsonify(x, max_depth) for x in o)
+        return (jsonify(x, max_depth=max_depth, parse_enums=parse_enums) for x in o)
+    elif issubclass(o, Enum):
+        if parse_enums == PARSE_NAME:
+            return o.name
+        elif parse_enums == PARSE_NAME_LONG:
+            return str(o)
+        elif parse_enums == PARSE_VALUE:
+            return o.value
+        elif parse_enums == PARSE_BOTH:
+            return {'name': o.name, 'value': o.value}
     return o
 
 
